f4f7171c6ac7328731dc692507e92ce5c93549e7
jersonvm <jerson.millora@gmail.com>
2024-09-08 14:46:39 +0800

    First commit for Assignment 6 Part 1

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -14,6 +14,9 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <linux/fs.h>
+#include <pthread.h>
+#include "queue.h"
+#include <time.h>
 
 #define PORT "9000" // the port users will be connecting to
 #define BACKLOG 10 // how many pending connections queue will hold
@@ -22,28 +25,41 @@
 
 
 bool is_signal_caught = false;
+static pthread_mutex_t the_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct slist_data_s {
+	pthread_t thread;
+	bool thread_complete;
+	int new_sockfd;
+	SLIST_ENTRY(slist_data_s) entries;
+};
 
 
-static void sig_handler(int signo)
-{
+static void sig_handler(int signo) {
     if (signo == SIGINT || signo == SIGTERM) {
         is_signal_caught = true;
     }
 }
 
-int receive(int new_fd ) {
+//int receive(int new_fd ) {
+void *receive_thread( void *arg ) {
+	struct slist_data_s *threadp = (struct slist_data_s *)arg;
+	int new_fd = threadp->new_sockfd;
+
 	char buf[BUFSIZE];
 	int  err, ret, file;
-	int rc = 0;
+	//int rc = 0;
 	
 	// ready to communicate on socket descriptor new_fd!
 	memset(buf, 0, BUFSIZE);
 	
+	
+	
 	if((file = open(FILEPATH, O_CREAT | O_APPEND | O_RDWR, 0644)) == -1) {
 		err = errno;
-		perror("open error:");
+		perror("open");
 		syslog(LOG_ERR, "open error: %s\n", strerror(err));
-		rc = -1;
+		//rc = -1;
 	}
 	
 	while((ret = recv(new_fd, buf, BUFSIZE-1, 0)) > 0) {
@@ -52,38 +68,43 @@ int receive(int new_fd ) {
 		char *newline = strchr(buf, '\n');
 		
 		if (newline) {
+			pthread_mutex_lock(&the_mutex);
 			if(write(file, buf, newline - buf + 1) < 0) {
 				err = errno;
-				perror("write error:");
+				perror("write");
 				syslog(LOG_ERR, "write error: %s\n", strerror(err));
-				rc = -1;
-				break;
+				//rc = -1;
+				//break;
 			} else {
 				//printf("1writing %s to %s\n", buf, FILEPATH);
 				syslog(LOG_DEBUG, "writing %s to %s", buf, FILEPATH);
-				break;
+				//break;
 			}
+			pthread_mutex_unlock(&the_mutex);
+			break;
 		}
 		else {
+            		pthread_mutex_lock(&the_mutex);
             		if(write(file, buf, ret) < 0) {
 				err = errno;
-				perror("write error:");
+				perror("write");
 				syslog(LOG_ERR, "write error: %s\n", strerror(err));
-				break;
-				rc = -1;
+				//break;
+				//rc = -1;
 			} else {
 				//printf("2writing %s to %s\n", buf, FILEPATH);
 				syslog(LOG_DEBUG, "writing %s to %s", buf, FILEPATH);
 			}
+			pthread_mutex_unlock(&the_mutex);
 		}
 
 	}
 	
 	if (ret == -1) {
 		err = errno;
-		perror("recv error:");
+		perror("recv");
 		syslog(LOG_ERR, "recv error: %s\n", strerror(err));
-		rc = -1;
+		//rc = -1;
 	}
 	
 	lseek(file, 0, SEEK_SET);
@@ -93,9 +114,9 @@ int receive(int new_fd ) {
         
         	if (send(new_fd, buf, ret, 0) < 0) {
 			err = errno;
-			perror("read error:");
+			perror("read");
 			syslog(LOG_ERR, "read error: %s\n", strerror(err));
-			rc = -1;
+			//rc = -1;
 			break;
 		}
 
@@ -103,8 +124,50 @@ int receive(int new_fd ) {
     	}
 	
 	close(file);
+	threadp->thread_complete = true;
 	close(new_fd);
-	return rc;
+	//return rc;
+	return arg;
+}
+
+
+void timer_thread() {
+	//https://www.geeksforgeeks.org/strftime-function-in-c/
+	time_t t ;
+	struct tm *tmp ;
+	char MY_TIME[50];
+	time( &t );
+	
+	memset(MY_TIME, 0, 50);
+	
+	//localtime() uses the time pointed by t ,
+	// to fill a tm structure with the 
+	// values that represent the 
+	// corresponding local time.
+     
+	tmp = localtime( &t );
+     
+	// using strftime to display time
+	strftime(MY_TIME, sizeof(MY_TIME), "timestamp: %Y %B %d %T\n", tmp);
+	
+	int  err, file;
+	
+	if((file = open(FILEPATH, O_CREAT | O_APPEND | O_RDWR, 0644)) == -1) {
+		err = errno;
+		perror("open");
+		syslog(LOG_ERR, "open error: %s\n", strerror(err));
+	}
+	
+	pthread_mutex_lock(&the_mutex);
+	if(write(file, MY_TIME, strlen(MY_TIME)) < 0) {
+		err = errno;
+		perror("write");
+		syslog(LOG_ERR, "write error: %s\n", strerror(err));
+	}
+	pthread_mutex_unlock(&the_mutex);
+	
+	close(file);
+	
 }
 
 // get sockaddr, IPv4 or IPv6:
@@ -134,9 +197,9 @@ int main(int argc, char *argv[]) {
 		}
 	}
 	
-	//if(open(FILEPATH, O_CREAT | O_APPEND | O_RDWR, 0644) > 0) {
-	//	remove(FILEPATH);
-	//}
+	if(open(FILEPATH, O_CREAT | O_APPEND | O_RDWR, 0644) > 0) {
+		remove(FILEPATH);
+	}
 	
 	memset(&hints, 0, sizeof hints); // make sure the struct is empty
 	hints.ai_family = AF_INET; // use IPv4
@@ -154,7 +217,7 @@ int main(int argc, char *argv[]) {
 	// make a socket, bind it, and listen on it:
 	if ((sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) == -1) {
 		err = errno;
-		perror("socket error:");
+		perror("socket");
 		syslog(LOG_ERR, "socket error: %s\n", strerror(err));
 		rc = -1;
 	}
@@ -162,7 +225,7 @@ int main(int argc, char *argv[]) {
 	int yes=1;
 	if (setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof yes) == -1) {
 		err = errno;
-		perror("setsockopt error:");
+		perror("setsockopt");
 		syslog(LOG_ERR, "vs error: %s\n", strerror(err));
 		rc = -1;
 	}
@@ -170,14 +233,14 @@ int main(int argc, char *argv[]) {
 
 	if ((ret = bind(sockfd, res->ai_addr, res->ai_addrlen)) == -1) {
 		err = errno;
-		perror("bind error:");
+		perror("bind");
 		syslog(LOG_ERR, "bind error: %s\n", strerror(err));
 		rc = -1;
 		is_daemon_true = false;
 	}
 	
 	if(is_daemon_true) {
-		// steps discussed in LSP page 173 to 174
+		// steps discussed in LSP pages 173 to 174
 		
 		// create a new process
 		pid = fork();
@@ -217,9 +280,33 @@ int main(int argc, char *argv[]) {
 				
 	}
 	
+	//LSP pages 389 to 394
+	timer_t timer;
+	struct sigevent evp;
+	struct itimerspec ts;
+	
+	evp.sigev_value.sival_ptr = &timer;
+	evp.sigev_notify = SIGEV_THREAD;
+	evp.sigev_notify_function = timer_thread;
+	evp.sigev_notify_attributes = NULL;
+	
+	ts.it_interval.tv_sec = 10;
+	ts.it_interval.tv_nsec = 0;
+	ts.it_value.tv_sec = 10;
+	ts.it_value.tv_nsec = 0;
+	
+	if(timer_create(CLOCK_REALTIME, &evp, &timer) == -1) {
+		perror("timer_create");
+	}
+	
+	if(timer_settime(timer, 0, &ts, NULL) == -1) {
+		perror("timer_settime");
+	}
+	
+	
 	if ((ret = listen(sockfd, BACKLOG)) == -1) {
 		err = errno;
-		perror("listen error:");
+		perror("listen");
 		syslog(LOG_ERR, "listen error: %s\n", strerror(err));
 		rc = -1;
 	}
@@ -231,17 +318,22 @@ int main(int argc, char *argv[]) {
 
 	if (sigaction(SIGINT, &sig, NULL) == -1) {
 		err = errno;
-		perror("sigaction error:");
+		perror("sigaction");
 		syslog(LOG_ERR, "sigaction error: %s\n", strerror(err));
 		rc = -1;
 	}
 
 	if (sigaction(SIGTERM, &sig, NULL) == -1) {
 		err = errno;
-		perror("sigaction error:");
+		perror("sigaction");
 		syslog(LOG_ERR, "sigaction error: %s\n", strerror(err));
 		rc = -1;
 	}
+	
+	
+	SLIST_HEAD(slisthead, slist_data_s) head;
+    	SLIST_INIT(&head);
+    	
 
 	
 	while(!is_signal_caught) {
@@ -253,7 +345,7 @@ int main(int argc, char *argv[]) {
 		addr_size = sizeof their_addr;
 		if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size)) == -1) {
 			err = errno;
-			perror("accept error:");
+			perror("accept:");
 			syslog(LOG_ERR, "accept error: %s\n", strerror(err));
 			rc = -1;
 		} else {
@@ -267,20 +359,37 @@ int main(int argc, char *argv[]) {
 			syslog(LOG_DEBUG, "Accepted connection from %s\n", ipstr);
 					
 					
-			ret = receive(new_fd);
-			close(new_fd);
+			//ret = receive(new_fd);
 			
-			printf("Closed connection from %s\n", ipstr);
-			syslog(LOG_DEBUG, "Closed connection from %s\n", ipstr);
-	
+			struct slist_data_s *threadp;
+			//threadp = calloc(sizeof(struct slist_data_s), 1);
+			threadp = malloc(sizeof(struct slist_data_s));
+			threadp->thread_complete = false;
+			threadp->new_sockfd = new_fd;
+			SLIST_INSERT_HEAD(&head, threadp, entries);
+			pthread_create(&threadp->thread, NULL, receive_thread, threadp);
+			
+			//close(new_fd);
+			//printf("Closed connection from %s\n", ipstr);
+			//syslog(LOG_DEBUG, "Closed connection from %s\n", ipstr);
+			
+			SLIST_FOREACH(threadp, &head, entries) {
+				//printf("Thread ID: %d, Completed: %d\n", (int)threadp->thread, threadp->thread_complete);
+				if(threadp->thread_complete == true) {
+					pthread_join(threadp->thread, NULL);
+				}
+			}
+			
+			free(threadp);
 					
 			if(ret == -1) {
-				printf("receive error\n");
+				//printf("receive error\n");
 				syslog(LOG_DEBUG, "receive error\n");
 				rc = -1;
 			}
 		}
 	}
+	
 
 	if(open(FILEPATH, O_CREAT | O_APPEND | O_RDWR, 0644) > 0) {
 		remove(FILEPATH);
@@ -291,6 +400,7 @@ int main(int argc, char *argv[]) {
 	// ... do everything until you don't need servinfo anymore ....
 	freeaddrinfo(res); // free the linked-list
 	
+	timer_delete(timer);
 	shutdown(sockfd, SHUT_RDWR);
 	close(new_fd);
 	close(sockfd);
